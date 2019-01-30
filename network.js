09ab556a84dd5bff7b92bd32562ad74842be795a
Kyle Parisi <kyleparisi@gmail.com>
2019-01-29 20:06:05 -0500

    Leaner network file

--- network.js
+++ network.js
@@ -1,69 +1,315 @@
-const data = [{"links":{"e826ddf0-204e-11e9-a9d4-bd73af05ca9d":{"direction":"->","source":{"node":"81dd85e0-204d-11e9-8a5f-8122a72dc5f4","port":"true"},"target":{"node":"d1432fd0-204e-11e9-a9d4-bd73af05ca9d","port":"data"},"id":"e826ddf0-204e-11e9-a9d4-bd73af05ca9d","layer_id":0},"34d880e0-204f-11e9-a9d4-bd73af05ca9d":{"direction":"->","source":{"node":"d1432fd0-204e-11e9-a9d4-bd73af05ca9d","port":"data"},"target":{"node":"1be098c0-204f-11e9-a9d4-bd73af05ca9d","port":"data"},"id":"34d880e0-204f-11e9-a9d4-bd73af05ca9d","layer_id":0},"ad2f4310-20a1-11e9-af01-637ab5bb2d38":{"direction":"->","source":{"node":"748e1630-2042-11e9-8a5f-8122a72dc5f4","port":"pages"},"target":{"node":"591c2130-20a1-11e9-af01-637ab5bb2d38","port":"data"},"id":"ad2f4310-20a1-11e9-af01-637ab5bb2d38","layer_id":0},"b1ca9d20-20a1-11e9-af01-637ab5bb2d38":{"direction":"->","source":{"node":"591c2130-20a1-11e9-af01-637ab5bb2d38","port":"data"},"target":{"node":"81dd85e0-204d-11e9-8a5f-8122a72dc5f4","port":"pages"},"id":"b1ca9d20-20a1-11e9-af01-637ab5bb2d38","layer_id":0,"logging":false},"dddba9e0-217d-11e9-a3d0-d5b60bade254":{"direction":"->","source":{"node":"81dd85e0-204d-11e9-8a5f-8122a72dc5f4","port":"false"},"target":{"node":"5387fb40-217d-11e9-a3d0-d5b60bade254","port":"data"},"id":"dddba9e0-217d-11e9-a3d0-d5b60bade254","layer_id":0},"df3a9da0-217d-11e9-a3d0-d5b60bade254":{"direction":"->","source":{"node":"5387fb40-217d-11e9-a3d0-d5b60bade254","port":"data"},"target":{"node":"1ebbcba0-20a4-11e9-8401-3ff0562c2761","port":"data"},"id":"df3a9da0-217d-11e9-a3d0-d5b60bade254","layer_id":0},"a55eebf0-2181-11e9-b98c-ef62f10eaa97":{"direction":"->","source":{"node":"81dd85e0-204d-11e9-8a5f-8122a72dc5f4","port":"false"},"target":{"node":"97912380-2181-11e9-b98c-ef62f10eaa97","port":"data"},"id":"a55eebf0-2181-11e9-b98c-ef62f10eaa97","layer_id":0},"db0beb40-2181-11e9-8f30-afebba4f4d75":{"direction":"->","source":{"node":"97912380-2181-11e9-b98c-ef62f10eaa97","port":"data"},"target":{"node":"c2d88ba0-2181-11e9-b98c-ef62f10eaa97","port":"data"},"id":"db0beb40-2181-11e9-8f30-afebba4f4d75","layer_id":0},"49e6d340-2182-11e9-8f30-afebba4f4d75":{"direction":"->","source":{"node":"c2d88ba0-2181-11e9-b98c-ef62f10eaa97","port":"data"},"target":{"node":"1be098c0-204f-11e9-a9d4-bd73af05ca9d","port":"data"},"id":"49e6d340-2182-11e9-8f30-afebba4f4d75","layer_id":0,"logging":false},"14e27a90-2183-11e9-a5ca-c33d89b1c6f7":{"direction":"->","source":{"node":"5387fb40-217d-11e9-a3d0-d5b60bade254","port":"false"},"target":{"node":"aa578010-20a3-11e9-8401-3ff0562c2761","port":"data"},"id":"14e27a90-2183-11e9-a5ca-c33d89b1c6f7","layer_id":0},"16c8edd0-2183-11e9-a5ca-c33d89b1c6f7":{"direction":"->","source":{"node":"97912380-2181-11e9-b98c-ef62f10eaa97","port":"false"},"target":{"node":"aa578010-20a3-11e9-8401-3ff0562c2761","port":"data"},"id":"16c8edd0-2183-11e9-a5ca-c33d89b1c6f7","layer_id":0},"9690dfa0-2183-11e9-a5ca-c33d89b1c6f7":{"direction":"->","source":{"node":"6a572fb0-20bc-11e9-ba36-41184c4609c5","port":"data"},"target":{"node":"1be098c0-204f-11e9-a9d4-bd73af05ca9d","port":"data"},"id":"9690dfa0-2183-11e9-a5ca-c33d89b1c6f7","layer_id":0},"ecf2ac70-2183-11e9-a2cd-d943fbf3448c":{"direction":"->","source":{"node":"aa578010-20a3-11e9-8401-3ff0562c2761","port":"data"},"target":{"node":"6a572fb0-20bc-11e9-ba36-41184c4609c5","port":"data"},"id":"ecf2ac70-2183-11e9-a2cd-d943fbf3448c","layer_id":0},"393bd420-2185-11e9-94b4-a3e8d4f4dc01":{"direction":"->","source":{"node":"1ebbcba0-20a4-11e9-8401-3ff0562c2761","port":"data"},"target":{"node":"1be098c0-204f-11e9-a9d4-bd73af05ca9d","port":"data"},"id":"393bd420-2185-11e9-94b4-a3e8d4f4dc01","layer_id":0}}}];
-const Channel = require("async-csp").Channel;
-const engine = {
-  outputs: {},
-  inputs: {}
-};
-// Links
-data.map(layer => {
-  const links = layer.links;
-  Object.keys(links).map(link_id => {
-    const link = links[link_id];
-    const input = new Channel();
-    const output = new Channel();
-    const { source, target, direction = "->" } = link;
+module.exports = function() {
+  const data = [
+    {
+      links: {
+        "1": {
+          direction: "->",
+          source: {
+            node: "2",
+            port: "true"
+          },
+          target: {
+            node: "3",
+            port: "data"
+          },
+          id: "1",
+          layer_id: 0
+        },
+        "4": {
+          direction: "->",
+          source: {
+            node: "3",
+            port: "data"
+          },
+          target: {
+            node: "5",
+            port: "data"
+          },
+          id: "4",
+          layer_id: 0
+        },
+        "6": {
+          direction: "->",
+          source: {
+            node: "7",
+            port: "pages"
+          },
+          target: {
+            node: "8",
+            port: "data"
+          },
+          id: "6",
+          layer_id: 0
+        },
+        "9": {
+          direction: "->",
+          source: {
+            node: "8",
+            port: "data"
+          },
+          target: {
+            node: "2",
+            port: "pages"
+          },
+          id: "9",
+          layer_id: 0,
+          logging: false
+        },
+        "10": {
+          direction: "->",
+          source: {
+            node: "2",
+            port: "false"
+          },
+          target: {
+            node: "11",
+            port: "data"
+          },
+          id: "10",
+          layer_id: 0
+        },
+        "12": {
+          direction: "->",
+          source: {
+            node: "11",
+            port: "data"
+          },
+          target: {
+            node: "14",
+            port: "data"
+          },
+          id: "12",
+          layer_id: 0
+        },
+        "13": {
+          direction: "->",
+          source: {
+            node: "2",
+            port: "false"
+          },
+          target: {
+            node: "15",
+            port: "data"
+          },
+          id: "13",
+          layer_id: 0
+        },
+        "16": {
+          direction: "->",
+          source: {
+            node: "15",
+            port: "data"
+          },
+          target: {
+            node: "17",
+            port: "data"
+          },
+          id: "16",
+          layer_id: 0
+        },
+        "18": {
+          direction: "->",
+          source: {
+            node: "17",
+            port: "data"
+          },
+          target: {
+            node: "5",
+            port: "data"
+          },
+          id: "18",
+          layer_id: 0,
+          logging: false
+        },
+        "19": {
+          direction: "->",
+          source: {
+            node: "11",
+            port: "false"
+          },
+          target: {
+            node: "20",
+            port: "data"
+          },
+          id: "19",
+          layer_id: 0
+        },
+        "21": {
+          direction: "->",
+          source: {
+            node: "15",
+            port: "false"
+          },
+          target: {
+            node: "20",
+            port: "data"
+          },
+          id: "21",
+          layer_id: 0
+        },
+        "22": {
+          direction: "->",
+          source: {
+            node: "23",
+            port: "data"
+          },
+          target: {
+            node: "5",
+            port: "data"
+          },
+          id: "22",
+          layer_id: 0
+        },
+        "24": {
+          direction: "->",
+          source: {
+            node: "20",
+            port: "data"
+          },
+          target: {
+            node: "23",
+            port: "data"
+          },
+          id: "24",
+          layer_id: 0
+        },
+        "25": {
+          direction: "->",
+          source: {
+            node: "14",
+            port: "data"
+          },
+          target: {
+            node: "5",
+            port: "data"
+          },
+          id: "25",
+          layer_id: 0
+        }
+      }
+    }
+  ];
+  const Channel = function() {
+    return new Proxy(
+      {
+        takes: [],
+        puts: []
+      },
+      {
+        get: function(obj, prop) {
+          if (prop === "take") {
+            // if puts ready (upstream)
+            if (obj.puts.length) {
+              return () => obj.puts.shift();
+            }
+            // no puts ready (might happen downstream)
+            let ref;
+            const prom = new Promise(resolve => (ref = resolve));
+            obj.takes.push(ref);
+            return () => prom;
+          }
+          if (prop === "put") {
+            // if takes ready (upstream)
+            if (obj.takes.length) {
+              return obj.takes.shift();
+            }
+            // no takes ready (might happen downstream);
+            const fn = data => {
+              obj.puts.push(new Promise(resolve => resolve(data)));
+            };
+            return fn;
+          }
+        }
+      }
+    );
+  };
+  const engine = {
+    outputs: {},
+    inputs: {}
+  };
+  // Links
+  data.map(layer => {
+    const links = layer.links;
+    Object.keys(links).map(link_id => {
+      const link = links[link_id];
+      const input = new Channel();
+      const output = new Channel();
+      const { source, target, direction = "->" } = link;
 
-    // animation of packet, one direction
-    if (direction === "->") {
-      async function leftToRightmiddleware() {
-        let outputData = await output.take();
+      // animation of packet, one direction
+      if (direction === "->") {
+        async function leftToRightmiddleware() {
+          let outputData = await output.take();
 
-        if (outputData === Channel.DONE) {
-          return false;
+          if (typeof outputData === "symbol") {
+            console.log("link closed");
+            return false;
+          }
+
+          if (link.logging) {
+            console.log(outputData);
+          }
+          // input.put(JSON.parse(JSON.stringify(outputData)));
+          input.put(outputData);
+
+          leftToRightmiddleware.call(this).catch(() => {});
         }
 
-        input.put(outputData);
-
-        leftToRightmiddleware.call(this).catch(() => {});
+        leftToRightmiddleware.call(this).catch(console.log);
       }
 
-      leftToRightmiddleware.call(this).catch(console.log);
-    }
+      // output side of link
+      if (
+        !!(
+          engine.outputs[source.node] &&
+          engine.outputs[source.node][source.port]
+        )
+      ) {
+        // fork channel
+        const pipe = new Channel();
+        const originalChan = engine.outputs[source.node][source.port];
+        (async () => {
+          while (pipe) {
+            const data = await pipe.take();
+            originalChan.put(data);
+            output.put(data);
+          }
+        })();
+        engine.outputs[source.node][source.port] = pipe;
+      } else {
+        // new channel
+        engine.outputs[source.node] = engine.outputs[source.node] || {};
+        engine.outputs[source.node][source.port] = output;
+      }
 
-    // output side of link
-    if (
-      !!(
-        engine.outputs[source.node] && engine.outputs[source.node][source.port]
-      )
-    ) {
-      // fork channel
-      const piper = new Channel();
-      piper.pipe(engine.outputs[source.node][source.port], output);
-      engine.outputs[source.node][source.port] = piper;
-    } else {
-      // new channel
-      engine.outputs[source.node] = engine.outputs[source.node] || {};
-      engine.outputs[source.node][source.port] = output;
-    }
+      // input side of link
+      if (
+        engine.inputs[target.node] &&
+        engine.inputs[target.node][target.port]
+      ) {
+        const newChannel = new Channel();
+        const originalChannel = engine.inputs[target.node][target.port];
+        engine.inputs[target.node][target.port] = newChannel;
 
-    // input side of link
-    if (engine.inputs[target.node] && engine.inputs[target.node][target.port]) {
-      // merge channels
-      engine.inputs[target.node][target.port] = engine.inputs[target.node][
-        target.port
-        ].merge(input);
-    } else {
-      // new channel
-      engine.inputs[target.node] = engine.inputs[target.node] || {};
-      engine.inputs[target.node][target.port] = input;
-    }
+        (async () => {
+          const data = await originalChannel.take();
+          engine.inputs[target.node][target.port].put(data);
+        })();
+        (async () => {
+          const data = await input.take();
+          engine.inputs[target.node][target.port].put(data);
+        })();
+      } else {
+        // new channel
+        engine.inputs[target.node] = engine.inputs[target.node] || {};
+        engine.inputs[target.node][target.port] = input;
+      }
+    });
   });
-});
 
-module.exports = function() {
   (async () => {
-    const inputs = engine.inputs["748e1630-2042-11e9-8a5f-8122a72dc5f4"];
-    const outputs = engine.outputs["748e1630-2042-11e9-8a5f-8122a72dc5f4"];
+    const inputs = engine.inputs["7"];
+    const outputs = engine.outputs["7"];
 
     if (arguments.length) {
       outputs.pages.put({ pages: arguments[0], currentPage: arguments[1] });
@@ -72,8 +318,8 @@ module.exports = function() {
     }
   })(arguments);
   (async () => {
-    const inputs = engine.inputs["81dd85e0-204d-11e9-8a5f-8122a72dc5f4"];
-    const outputs = engine.outputs["81dd85e0-204d-11e9-8a5f-8122a72dc5f4"];
+    const inputs = engine.inputs["2"];
+    const outputs = engine.outputs["2"];
 
     const pages = await inputs.pages.take();
     if (pages.pages.length <= 7 && outputs.true) {
@@ -83,8 +329,8 @@ module.exports = function() {
     }
   })(arguments);
   (async () => {
-    const inputs = engine.inputs["d1432fd0-204e-11e9-a9d4-bd73af05ca9d"];
-    const outputs = engine.outputs["d1432fd0-204e-11e9-a9d4-bd73af05ca9d"];
+    const inputs = engine.inputs["3"];
+    const outputs = engine.outputs["3"];
 
     const data = await inputs.data.take();
     const view = [];
@@ -95,8 +341,8 @@ module.exports = function() {
     outputs.data.put(data);
   })(arguments);
   (async () => {
-    const inputs = engine.inputs["1be098c0-204f-11e9-a9d4-bd73af05ca9d"];
-    const outputs = engine.outputs["1be098c0-204f-11e9-a9d4-bd73af05ca9d"];
+    const inputs = engine.inputs["5"];
+    const outputs = engine.outputs["5"];
 
     const data = await inputs.data.take();
     try {
@@ -106,8 +352,8 @@ module.exports = function() {
     }
   })(arguments);
   (async () => {
-    const inputs = engine.inputs["591c2130-20a1-11e9-af01-637ab5bb2d38"];
-    const outputs = engine.outputs["591c2130-20a1-11e9-af01-637ab5bb2d38"];
+    const inputs = engine.inputs["8"];
+    const outputs = engine.outputs["8"];
 
     const data = await inputs.data.take();
     if (!data.pages || !data.pages.length) {
@@ -129,16 +375,16 @@ module.exports = function() {
     outputs.data.put(data);
   })(arguments);
   (async () => {
-    const inputs = engine.inputs["aa578010-20a3-11e9-8401-3ff0562c2761"];
-    const outputs = engine.outputs["aa578010-20a3-11e9-8401-3ff0562c2761"];
+    const inputs = engine.inputs["20"];
+    const outputs = engine.outputs["20"];
 
     await inputs.data.take();
     const data = await inputs.data.take();
     outputs.data.put(data);
   })(arguments);
   (async () => {
-    const inputs = engine.inputs["1ebbcba0-20a4-11e9-8401-3ff0562c2761"];
-    const outputs = engine.outputs["1ebbcba0-20a4-11e9-8401-3ff0562c2761"];
+    const inputs = engine.inputs["14"];
+    const outputs = engine.outputs["14"];
 
     const data = await inputs.data.take();
     const { pages } = data;
@@ -152,8 +398,8 @@ module.exports = function() {
     outputs.data.put(data);
   })(arguments);
   (async () => {
-    const inputs = engine.inputs["6a572fb0-20bc-11e9-ba36-41184c4609c5"];
-    const outputs = engine.outputs["6a572fb0-20bc-11e9-ba36-41184c4609c5"];
+    const inputs = engine.inputs["23"];
+    const outputs = engine.outputs["23"];
 
     const data = await inputs.data.take();
     const { currentPage, pages } = data;
@@ -166,8 +412,8 @@ module.exports = function() {
     outputs.data.put(data);
   })(arguments);
   (async () => {
-    const inputs = engine.inputs["5387fb40-217d-11e9-a3d0-d5b60bade254"];
-    const outputs = engine.outputs["5387fb40-217d-11e9-a3d0-d5b60bade254"];
+    const inputs = engine.inputs["11"];
+    const outputs = engine.outputs["11"];
 
     const data = await inputs.data.take();
     if (data.pages.length - data.currentPage <= 3) {
@@ -177,8 +423,8 @@ module.exports = function() {
     }
   })(arguments);
   (async () => {
-    const inputs = engine.inputs["97912380-2181-11e9-b98c-ef62f10eaa97"];
-    const outputs = engine.outputs["97912380-2181-11e9-b98c-ef62f10eaa97"];
+    const inputs = engine.inputs["15"];
+    const outputs = engine.outputs["15"];
 
     const data = await inputs.data.take();
     if (data.currentPage <= 3) {
@@ -188,8 +434,8 @@ module.exports = function() {
     }
   })(arguments);
   (async () => {
-    const inputs = engine.inputs["c2d88ba0-2181-11e9-b98c-ef62f10eaa97"];
-    const outputs = engine.outputs["c2d88ba0-2181-11e9-b98c-ef62f10eaa97"];
+    const inputs = engine.inputs["17"];
+    const outputs = engine.outputs["17"];
 
     const data = await inputs.data.take();
     const render = [1, 2, 3, 4, 5, "...", data.pages.length];
